47b6f20a958eeb2352caa41abfc827f214e96b5a
Lucas Mendonça <p.lucas.santos.mendonca@gmail.com>
2025-09-10 21:47:43 -0300

    Ajuste nas regras

--- Auxilio-TO/Nova pasta/informacoes.py
+++ Auxilio-TO/Nova pasta/informacoes.py
@@ -46,120 +46,143 @@ Resposta: esticar pelo menos um dos braços ou mão e pelo menos uma das pernas 
 '''],
 '1 a 2 anos': ['''
 14 Come com colher de modo independente.
-Condição: o adulto poderá dar ajuda inicial, se necessário.
-Resposta: em cada colherada de comida ingerida, de modo independente, sem entornar excessivamente o alimento em cada colherada. Por dez tentativas em uma refeição.
+Condição: o adulto poderá dar ajuda à criança no final da refeição
+Resposta: comer com colher, de modo independente, sem derrubar excessivamente o alimento em cada colherada.
                
 15 Segura a caneca com uma só mão e bebe.
-Resposta: segurar a xícara com uma só mão e dar um gole (por tentativa) sem entornar excessivamente o líquido.               
+Resposta: segurar a xícara com uma só mão e dar um gole (por tentativa) sem derramar excessivamente o líquido.   
+                           
 16 Coloca a mão na água e dá tapinhas no rosto com as mãos molhadas, imitando alguém.
                
 17 Senta-se em um piniquinho ou sanitário infantil por 5 min.
-Condição: colocar a criança sentada no pinico ou privada infantil.               
+Condição: colocar a criança sentada no pinico ou privada infantil.    
+                          
 18 Coloca um chapéu na cabeça e o remove.
                
 19 Tira as meias.
-Resposta: retirar uma das meias por tentativa.               
+Resposta: retirar uma das meias por tentativa.
+                              
 20 Empurra os braços pelas mangas e os pés pelas pernas da calça.
-Condição: segurar firmemente a camisa, sendo que o braço da criança já foi colocado na cava da manga, de modo que própria criança execute o movimento de vesti-la. Fazer o mesmo com a calça por tentativa.               
+Condição: segurar firmemente a camisa, sendo que o braço da criança já foi colocado na cava da manga, de modo que própria criança execute o movimento de vestir-se. Fazer o mesmo com a calça por tentativa.
+                              
 21 Tira os sapatos quando os cordões estiverem desamarrados
-Resposta: retirar um dos sapatos por tentativa.               
+Resposta: retirar um dos sapatos por tentativa.
+                            
 22 Tira o casaco quando desabotoado.
                
 23 Tira a calça quando desabotoada.
                
 24 Puxa um fecho grande para cima e para baixo.
-Resposta: puxar um zíper grande, para cima e para baixo, que já esteja encaixado, sem esforço.               
+Resposta: puxar um zíper grande, para cima e para baixo, que já esteja encaixado, sem enroscá-lo na roupa. 
+                             
 25 Utiliza palavras ou gestos indicando necessidade de ir ao banheiro.
 Condição: entrevista com o mediador.
-Resposta: diz “xixi” ou “cocô” (ou emite aproximações destes sons) e aponta para o banheiro ou para calça/fralda, embora já possa estar molhada.
-Critério: no dia anterior, ter apresentado tal comportamento pelo menos duas vezes.               
+Resposta: diz “xixi” ou “cocô” (ou emite aproximações destes sons) e aponta para o banheiro ou para calça/fralda, embora já possa estar molhada.              
 '''],
 '2 a 3 anos': ['''
 26 Alimenta-se sozinho usando colher ou caneca, derrubando um pouco de comida ou derramando pouco líquido.
-Resposta: cada tentativa consiste em cada colherada, iniciada dentro do prato. Resposta satisfatória consiste em cada gole de um líquido sem derramar.
-Critério: alimentar-se de tal forma, pelo menos em dez tentativas de uma refeição.               
+Resposta: cada tentativa consiste em um gole e uma colherada, iniciada dentro de 15 segundos após a apresentação da bebida e do alimento.
+                              
 27 Quando recebe uma toalha enxuga as mãos e o rosto com ajuda.
-Condição: dar ajuda guiando a mão da criança.               
+Condição: dar ajuda guiando a mão da criança.
+                              
 28 Suga líquido do copo ou caneca usando canudinho.
-Resposta: bebe com canudinho sem engasgar, sem perder o fôlego, sem morder o canudinho e/ou projetar a língua.
-Critério: sugar todo o líquido de um copo ou xícara usando canudinho.             
+Resposta: bebe com canudinho sem engasgar, sem perder o fôlego, sem morder o canudinho e/ou sem projetar a língua.
+                            
 29 Dá garfadas.
 Condição: se necessário, dar instruções.
-Resposta: colocar alimento por garfada, levando uma porção até a boca por tentativa. Uma garfada deve ser iniciada dentro do prato; 15 segundos após apresentação do alimento.               
+Resposta: coloca alimento no garfo, levando uma porção até a boca por tentativa. Uma garfada deve ser iniciada dentro de 15 segundos após apresentação do alimento. 
+                             
 30 Mastiga e engole apenas substâncias comestíveis.
-Condição: apresentar à criança uma amostra de várias substâncias comestíveis e não comestíveis (terra, nabo ou planta perigosa, palha) e solicitar à criança que coloque na boca o que deve comer. Cada vez que mastigar e engolir uma substância não considerada comestível será considerada como uma tentativa. Se a criança levar à boca uma substância não considerada comestível, interromper sua ação antes de substância chegar à boca.               
+Condição: apresentar à criança uma amostra de várias substâncias comestíveis e não comestíveis (terra, mato ou planta pedregulho, tampinhas) e solicitar à criança que coloque na boca é de comer. Cada vez que a criança levar uma substância é considerada como uma tentativa. Se a criança levar à boca uma substância não comestível, interromper sua ação antes de a substância chegar à boca. 
+                             
 31 Enxuga as mãos sem ajuda ao lhe darem uma toalha.
-Critério: mãos secas.              
+                             
 32 Avisa que quer ir ao banheiro, mesmo sendo tarde demais.
-Condição: entrevista com o mediador.
-Resposta: dizer (no dia anterior), o que queria ir ao banheiro (embora tardiamente), pelo menos três vezes.              
+Condição: entrevista com o mediador. 
+                            
 33 Controla sua baba.
 Condição: entrevista com o mediador.
-Resposta: não necessitar do uso de babadores, não precisar que lhe enxuguem ou manter a camiseta/roupa seca.              
+Resposta: permanecer todo o período de vigília sem babar
+                            
 34 Urina ou defeca quando colocado no piniquinho pelo menos 3 vezes por semana.
-Condição: entrevista com o mediador. O mediador deverá colocar a criança no pinico/fralda pelo menos uma vez por dia.
-Critério: no mesmo dia, a criança deverá ter urinado ou defecado no pinico/pelo menos três vezes.              
+Condição: entrevista com o mediador. O mediador deverá colocar a criança no pinico pelo menos uma vez por dia.
+                            
 35 Calça os sapatos.
-Condição: cada tentativa envolve calçar adequadamente o par de sapatos, que já estejam desamarrados.
-Resposta: colocar cada um dos sapatos em seus respectivos pés, direito e esquerdo.             
+Condição: cada tentativa envolve calçar adequadamente o par de sapatos, que já devem estar previamente desabotados ou desamarrados.
+Resposta: colocar cada um dos sapatos em seu respectivo pé, direito e esquerdo.
+                            
 36 Escova os dentes imitando um adulto.
-Condição: abrir a torneira, dar para a criança escova com pasta e, em seguida, dar instruções, solicitando a resposta.
-Resposta: cada tentativa é constituída por pegar a escova na posição correta, molhar as cerdas da escova, movimentar as cerdas da escova pelos dentes e enxaguar a boca.              
+Condição: abrir a torneira, dar para a criança escova com pasta e, em seguida, dar o modelo, solicitando a resposta.
+Resposta: cada tentativa é constituída por pegar a escova na posição correta, molhar as cerdas da escova, movimentar as cerdas da escova pelos dentes e enxaguar a boca. 
+                            
 37 Retira roupas simples que foram desabotoadas.
                
 38 Usa o banheiro para defecar (falha apenas 1x por semana).
 Condição: entrevista com o mediador.
-Resposta: usar o banheiro para defecar, embora possa precisar de ajuda para tirar a roupa.
-Critério: na semana anterior, a criança deverá ter tido apenas um acidente diurno.              
+Resposta: usar o banheiro para defecar, embora possa precisar de ajuda para tirar a roupa, sentar e usar papel higiênico.
+                           
 39 Obtém água de uma torneira sem ajuda.
-Resposta: abrir a torneira, encher pelo menos 1/4 do copo com água e fechar a torneira.              
+Resposta: uma tentativa constitui em pegar o copo, abrir a torneira, encher pelo menos 1/4 do copo com água e fechar a torneira. 
+                            
 40 Lava as mãos e o rosto com um sabonete.
-Resposta: lavar as mãos – cada tentativa é constituída por molhar as mãos, pegar o sabonete e ensaboar as mãos.              
+Resposta: lavar as mãos = cada tentativa é constituída por molhar as mãos, pegar o sabonete, esfregr o sabonete nas mãos, colocar o sabonete em seu lugar, esfregar uma mão na outra e enxaguar as mãos. Lavar o rosto = cada tentativa é constituída por molhar as mãos, pegar o sabonete, esfregar o sabonete nas mãos, colocar o sabonete no lugar, passar as mãos ensaboadas no rosto, bochecha, queixo e testa e enxaguar o rosto.
+                            
 41 Avisa que quer ir ao banheiro durante o dia a tempo.
-Condição: entrevista com o mediador. O mediador deverá ter ajudado a criança (perguntando-lhe se quer ir ao banheiro) de modo que, pelo menos, uma vez por dia a criança vá ao banheiro.
-Resposta: pedir ou fazer gestos indicativos de ir ao banheiro (não vale apenas ficar puxando).
-Critério: nenhum acidente por dia durante a semana anterior.
+Condição: entrevista com o mediador. O mediador deverá ter ajudado a criança (encaminhando-a até o banheiro etc.) todas as vezes em que ela lhe "avisou" que queria ir ao banheiro.
+Resposta: pedir ou fazer gestos indicativos de ir ao banheiro (não vale apenas ficar "Pulando").
+
 42 Pendura o casaco em um gancho da sua altura.
                
 43 Permanece seco ao dormir durante o dia.
 Condição: entrevista com o mediador.
-Critério: ter acordado seco todas as vezes em que dormiu durante o dia naquela semana.             
+                            
 44 Evita riscos, por ex: pontas em móveis e escadas sem corrimão.
-Condição: entrevista com o mediador.            
+Condição: entrevista com o mediador.
+                           
 45 Usa guardanapo quando recomendado.
-Condição: sentar-se com o mediador. O mediador precisará já ter aconselhado a criança a usar guardanapo, o que deverá ser feito.
-Resposta: cada tentativa equivale a cada (lembrar de) usar guardanapo, o que deverá ser atendido de ordem, sendo que usar guardanapo = passar ou apresentar no rosto.             
+Condição: sentar-se com o mediador. O mediador precisaria já ter aconselhado a criança a usar guardanapo para que este item seja avaliado.
+Resposta: cada tentativa equivale a: dado o lembrete para usar guardanapo, a criança ter atendido à ordem, sendo que usar guardanapo = passar o guardanapo na boca. 
+                           
 46 Espeta o garfo na comida, levando-a a boca.
-Resposta: espetar o garfo dentro do prato e dentro de 15 segundos após apresentação do alimento.            
+Resposta: espetar o garfo dentro de 15 segundos após a apresentação do alimento.
+                        
 47 Despeja líquido de uma peq. jarra para o copo sem ajuda.
-Resposta: despejar líquido da jarra para o copo, sem derramá-lo excessivamente.               
+Resposta: despejar líquido da jarra para o copo, sem derramá-lo excessivamente.  
+                            
 48 Desprende roupas presas com o feixe de pressão.
-Condição: cada tentativa envolve a apresentação de roupa com um fecho de pressão.
-São acompanhadas de instruções.               
+Condição: cada tentativa envolve a apresentação de roupa com um fecho de pressão acompanhada de instruções.
+                              
 49 Lava seus braços e pernas ao lhe darem banho.
-Condição: lavar seus próprios braços e/ou pernas com espuma, movimentando sabonete ou esponja sobre o sabonete ou buchinha ou qualquer outro material espumando água.
-Resposta: passar sabonete (ou buchinha) ou esponja sobre parte do corpo já ensaboada, movendo-a.              
+Condição: entrevista com o mediador. O mediador precisa ter dado oportunidade à criança de lavar seus próprios braços e pernas.
+Resposta: passar sabonete e/ou bucha ou esponja com espuma, movimentando o sabonete ou esponja por todo o braço e perna, em seguida retirar a espuma jogando água.
+                             
 50 Coloca meias.
-Resposta: cada tentativa de zíper equivale ao par de meias.               
+Resposta: cada tentativa ao par de meias.  
+                            
 51 Veste casaco, malha ou camisa.
-Condição: dar camisa, casaco ou malha para a criança, podendo dar pistas sobre isto.
-Resposta: cada tentativa equivale ao colocar adequadamente um casaco, malha ou camisa.               
+Condição: dar camisa, casaco ou malha para a criança, podendo dar pistas sobre qual a frente da roupa ou qual o modo mais fácil de iniciar o movimento.
+Resposta: cada tentativa equivale a colocar adequadamente um casaco, malha ou camisa.  
+                            
 52 Identifica a parte dianteira da roupa. 
-Condição: mostrar uma peça de roupa entre suas mãos sinalizando frente e costas, pedindo para identificar qual é a parte dianteira da roupa.
-Resposta: identificar e apontar ou verbalizar qual é a frente da roupa.               
+Condição: a peça de roupa deve ter algum sinal tal como: botão, bolso, zíper etc. Que assinale a diferença entre as duas partes: dianteira e traseira.
+Resposta: identificar = apontar ou verbalizar qual a frente da roupa.
 '''],
 '3 a 4 anos':['''
 53 Alimenta-se sozinho por toda a refeição.
-Condição: comida deve estar no prato, cortada em pedaços pequenos. Entrevista com o mediador.
-Resposta: alimentar-se sozinho em toda a refeição.
+Condição: a comida deve estar no prato, cortada em pedaços pequenos. Entrevista com o mediador.
+Resposta: alimenta-se sozinha =  esvaziar o prato (ou a maior parte deste) levando à boca a colher/garfo de forma independente durante a refeição.
+              
 54 Veste-se só, precisando de ajuda apenas quanto há malhas ou camisetas com golas fechadas ou botões e fechos.
 Condição: entrevista com o mediador.
-Resposta: sentar-se corretamente (sem ficar torto ou se jogar).              
+Resposta: vestir-se sozinho = colocar cueca, e camisa ou calcinha e vestido, sem ajuda.
+                          
 55 Enxuga o nariz quando lembrado.
-Condição: criança em pé, sem apoio, é solicitado que se sente.
-Resposta: flexionar pernas e se sentar sem apoio externo.              
+Condição: propiciar lenço ou papel, quando o nariz da criança estiver escorrendo. Entrevista com o mediador.
+                            
 56 Acorda seco 2 manhãs por semana.
-Resposta: levantar-se sozinho de uma cadeira comum, sem apoio.              
+Condição: entrevista com o mediador
+                            
 57 Se menino, urina no sanitário, em pé.
 Resposta: levantar-se sozinho de posição baixa ou do chão.              
 58 Veste-se e despe-se sozinho, exceto quanto à botões e fechos em 75% das vezes.
